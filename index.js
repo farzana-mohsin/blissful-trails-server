eaa8df7d9526c347656c5f1333a8bd9793ef99b0
Farzana Mohsin <fmohsin@usertesting.com>
2024-06-02 20:54:05 -0400

    add get API to get indiviudal package details

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster81657.uygasmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster81657`;
 
@@ -24,8 +24,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const packagesCollection = client.db("tourDB").collection("packages");
+    // package related API
+    app.get("/packages", async (req, res) => {
+      const result = await packagesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/packages", async (req, res) => {
+      const newPackage = req.body;
+      const result = await packagesCollection.insertOne(newPackage);
+      res.send(result);
+    });
+
+    app.get("/packages/tour-type/:tourType", async (req, res) => {
+      const tourType = req.params.tourType;
+      const query = { tourType: tourType };
+      console.log("tour type");
+      const result = await packagesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const result = await packagesCollection.findOne(query);
+      res.send(result);
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -33,7 +62,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
